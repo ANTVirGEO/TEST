dffd1ffef4192b362b01257bdc0fe59d46b75b02
ANTVirGEO <virgili@yandex.ru>
2015-06-03 14:26:19 +0600

    Commit of 4 revision of 2 branch with TESTpassed  algoritm

---
+++ test.java
@@ -0,0 +1,68 @@
+package test;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
+public class test {
+
+    public static void main (String []args) {
+        int N;
+        int norm;
+        int k = 0;
+        System.out.println("Введите цифровое значение N массива ниже и нажмите Enter");
+        Scanner in = new Scanner(System.in);
+        try {
+            N = in.nextInt();
+            norm = 1;
+        } catch (Exception e) {
+            System.out.println("Ошибка вводного числа! : " + e + "\n Программа прекратила свою работу");
+            N = 0;
+            norm = 0;
+        }
+        if (N == 0) {
+            System.out.println("Массив не может быть равен нулю!");
+        } else {
+            if (norm == 1) {
+                System.out.println("Принятое значение N=" + N);
+                System.out.println("//////////Заполнение существующего массива для старта задания\\\\\\\\\\\\\\\\\\\\");
+                ArrayList<Integer> vah = new ArrayList<>();
+                for (int i = 1; vah.size() < (N) ; i++) {
+                    System.out.println(i);
+                    vah.add(i);
+                    System.out.println(vah);
+                }
+                System.out.println("Размер текущего массива=" + vah.size());
+                System.out.println("Внутренности массива:" + vah);
+                System.out.println("//////////Старт алгоритма\\\\\\\\\\\\\\\\\\\\");
+                Random rand = new Random();
+                for (int i = N; vah.size() < (N*2); i++) {
+                    int p = rand.nextInt((N)) + 1;
+                    int count = Collections.frequency(vah,i-N+1);
+                    System.out.println("Значение элемента массива= "+(i-N+1));
+                    System.out.println("Count of doubles= "+String.valueOf(count));
+                    System.out.println("p= "+p);
+                    if (count==1) {
+                        vah.add(p);
+                    }
+
+                    System.out.println(vah);
+                }
+                System.out.println("Новый размер массива: " + N * 2);
+                System.out.println("Вывод нового массива: " + vah);
+                Collections.shuffle(vah);
+                System.out.println("Вывод шуффла массива: " + vah);
+                int t = rand.nextInt((N * 2));
+                System.out.println("Рандомный индекс для удаления элемента массива: " + t);
+                k = vah.get(t);
+                vah.remove(t);
+                System.out.println("Вывод обрезанного массива: " + vah);
+                System.out.println("Вычлененный нами элемент массива содержал цифру= " + k);
+            }
+        }
+    }
+}
+
+
